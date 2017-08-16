eb904a38933f05a214a7448dc2afc93d8a8d175d
Daniel McDonald <mcdonadt@colorado.edu>
2017-08-16 16:46:05 -0700

    Citations

--- sucpp/su.cpp
+++ sucpp/su.cpp
@@ -19,6 +19,17 @@ void usage() {
     std::cout << "    -a\t\t[OPTIONAL] Generalized UniFrac alpha, default is 1." << std::endl;
     std::cout << "    --vaw\t[OPTIONAL] Variance adjusted, default is to not adjust for variance." << std::endl;
     std::cout << std::endl;
+    std::cout << "Citations: " << std::endl;
+    std::cout << "    For UniFrac, please see:" << std::endl;
+    std::cout << "        Lozupone and Knight Appl Environ Microbiol 2005; DOI: 10.1128/AEM.71.12.8228-8235.2005" << std::endl;
+    std::cout << "        Lozupone et al. Appl Environ Microbiol 2007; DOI: 10.1128/AEM.01996-06" << std::endl;
+    std::cout << "        Hamady et al. ISME 2010; DOI: 10.1038/ismej.2009.97" << std::endl;
+    std::cout << "        Lozupone et al. ISME 2011; DOI: 10.1038/ismej.2010.133" << std::endl;
+    std::cout << "    For Generalized UniFrac, please see: " << std::endl;
+    std::cout << "        Chen et al. Bioinformatics 2012; DOI: 10.1093/bioinformatics/bts342" << std::endl;
+    std::cout << "    For Variance Adjusted UniFrac, please see: " << std::endl;
+    std::cout << "        Chang et al. BMC Bioinformatics 2011; DOI: 10.1186/1471-2105-12-118" << std::endl;
+    std::cout << std::endl;
 }
 
 void err(std::string msg) {
