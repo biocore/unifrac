c84acaf19eb0d8cf16db7e83bd9661a9836e7c02
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2017-09-20 15:46:09 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -89,7 +89,7 @@ if USE_CYTHON:
 
 setup(
     name="unifrac",
-    version="0.9.0-dev",
+    version="0.9.0",
     packages=find_packages(),
     author="Daniel McDonald",
     license='BSD-3-Clause',
