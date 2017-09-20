34740a67770fe1d94b6a86a7940fde06d49d00f2
Daniel McDonald <mcdonadt@colorado.edu>
2017-09-20 11:48:06 -0700

    Addresses #46

--- sucpp/unifrac.cpp
+++ sucpp/unifrac.cpp
@@ -256,8 +256,7 @@ void su::unifrac(biom &table,
     initialize_embedded(embedded_proportions, task_p);
     initialize_stripes(std::ref(dm_stripes), std::ref(dm_stripes_total), unifrac_method, task_p);
 
-    // - 1 to avoid root   
-    for(unsigned int k = 0; k < (tree.nparens / 2) - 1; k++) {
+    for(unsigned int k = 0; k < (tree.nparens / 2); k++) {
         node = tree.postorderselect(k);
         length = tree.lengths[node];
 
@@ -386,8 +385,7 @@ void su::unifrac_vaw(biom &table,
     initialize_sample_counts(sample_total_counts, task_p, table);
     initialize_stripes(std::ref(dm_stripes), std::ref(dm_stripes_total), unifrac_method, task_p);
 
-    // - 1 to avoid root   
-    for(unsigned int k = 0; k < (tree.nparens / 2) - 1; k++) {
+    for(unsigned int k = 0; k < (tree.nparens / 2); k++) {
         node = tree.postorderselect(k);
         length = tree.lengths[node];
 
