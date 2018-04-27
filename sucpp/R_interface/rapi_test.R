87e86406c5c8484324ea29f47918efd4305cc476
Nicolai Reeve <nreeve@ucsd.edu>
2018-04-27 10:40:22 -0700

    test changes

--- sucpp/R_interface/rapi_test.R
+++ sucpp/R_interface/rapi_test.R
@@ -1,10 +1,14 @@
 library(Rcpp)
 
-equals <- function(x, y, ){
-	stopifnot(x==y)
+equals <- function(x, y, msg){
+    if (x!=y)
+        stop(msg)
+	#stopifnot(x==y)
 }
-aboutEquals <- function(x, y){
-	stopifnot((x-y)<0.005)
+aboutEquals <- function(x, y, msg){
+	if((x-y)>0.005)
+        stop(msg)
+    #stopifnot((x-y)<0.005)
 }
 source = "su_R.cpp"
 sourceCpp(source)
@@ -18,12 +22,12 @@ exp = c(0.2000000, 0.5714286, 0.6000000, 0.5000000, 0.2000000,
 		0.8571429, 0.4285714, 0.3333333, 0.4000000, 0.6000000)
 
 
-equals(unif["n_samples"][[1]], 6)
-equals(unif["cf_size"][[1]], 15)
-equals(unif["is_upper_triangle"][[1]], TRUE)
+equals(unif["n_samples"][[1]], 6, "n_samples != 6")
+equals(unif["cf_size"][[1]], 15, "cf_size != 15")
+equals(unif["is_upper_triangle"][[1]], TRUE, "is_upper_triagnle != TRUE")
 
 
 for ( i in 1:15){
-	aboutEquals(unif["c_form"][[1]][i], exp[i])
+	aboutEquals(unif["c_form"][[1]][i], exp[i], "Output not as expected")
 }
 print('All tests pass')
