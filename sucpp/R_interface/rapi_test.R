81f1a5399654adc686014431f0aa8fb97d383717
Nicolai Reeve <nreeve@ucsd.edu>
2018-04-27 10:42:54 -0700

    matching to issue-53, i should not have opened two separate issues. I believe the reasoning was that one was creating special places for all the different wrappers whereas this branch was only supposed to be for the R-wrapper, but i ended up making more changes to the other branch

--- sucpp/R_interface/rapi_test.R
+++ sucpp/R_interface/rapi_test.R
@@ -1,20 +1,17 @@
 library(Rcpp)
 
-equals <- function(x, y){
-	if (x == y){
-		return(TRUE)
-	}else {
-		return(FALSE) 
-	}
+equals <- function(x, y, msg){
+    if (x!=y)
+        stop(msg)
+	#stopifnot(x==y)
 }
-aboutEquals <- function(x, y){
-	if ((x-y)<0.05){
-		return(TRUE)
-	}else{
-		return(FALSE)
-	}
+aboutEquals <- function(x, y, msg){
+	if((x-y)>0.005)
+        stop(msg)
+    #stopifnot((x-y)<0.005)
 }
-sourceCpp("su_R.cpp")
+source = "su_R.cpp"
+sourceCpp(source)
 table = "test.biom"
 tree = "test.tre"
 nthreads = 2
@@ -25,12 +22,13 @@ exp = c(0.2000000, 0.5714286, 0.6000000, 0.5000000, 0.2000000,
 		0.8571429, 0.4285714, 0.3333333, 0.4000000, 0.6000000)
 
 
-equals(unif["n_samples"][[1]], 6)
-equals(unif["cf_size"][[1]], 15)
-equals(unif["is_upper_triangle"][[1]], TRUE)
+equals(unif["n_samples"][[1]], 6, "n_samples != 6")
+equals(unif["cf_size"][[1]], 15, "cf_size != 15")
+equals(unif["is_upper_triangle"][[1]], TRUE, "is_upper_triagnle != TRUE")
 
-print("testing values of c_form")
+
 for ( i in 1:15){
-	print(aboutEquals(unif["c_form"][[1]][i], exp[i]))
+	aboutEquals(unif["c_form"][[1]][i], exp[i], "Output not as expected")
 }
+print('All tests pass')
 
