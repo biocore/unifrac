6d27be4a87d37a672b0a0f95dad5794ae85a0af3
Igor Sfiligoi <isfiligoi@sdsc.edu>
2020-12-15 12:42:35 -0800

    Add python interface to ssu_to_file. Also properly expose _fp32 methods

--- unifrac/__init__.py
+++ unifrac/__init__.py
@@ -11,10 +11,18 @@ import pkg_resources
 from unifrac._methods import (unweighted,
                               weighted_normalized,
                               weighted_unnormalized,
-                              generalized, meta)
-from unifrac._api import ssu, faith_pd
+                              generalized,
+                              unweighted_fp32,
+                              weighted_normalized_fp32,
+                              weighted_unnormalized_fp32,
+                              generalized_fp32, 
+                              meta)
+from unifrac._api import ssu, faith_pd, ssu_to_file
 
 
 __version__ = pkg_resources.get_distribution('unifrac').version
 __all__ = ['unweighted', 'weighted_normalized', 'weighted_unnormalized',
-           'generalized', 'meta', 'ssu', 'faith_pd']
+           'generalized', 'unweighted_p32', 'weighted_normalized_fp32',
+           'weighted_unnormalized_fp32', 'generalized_fp32',
+           'meta', 
+           'ssu', 'faith_pd', 'ssu_to_file']
