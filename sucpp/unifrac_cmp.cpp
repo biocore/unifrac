5d73af916c58fdc9d2e3189658e22717d90b48e3
Igor Sfiligoi <isfiligoi@ucsd.edu>
2021-06-30 14:28:02 -0700

    Fix typo

--- sucpp/unifrac_cmp.cpp
+++ sucpp/unifrac_cmp.cpp
@@ -341,7 +341,7 @@ inline void unifrac_vawTT(const su::biom_interface &table,
 #pragma acc parallel loop collapse(2) present(dm_stripes_buf,dm_stripes_total_buf)
         for(unsigned int i = start_idx; i < stop_idx; i++)
             for(unsigned int j = 0; j < n_samples; j++) {
-                unit64_t idx = (i-start_idx)*n_samples_r+j;
+                uint64_t idx = (i-start_idx)*n_samples_r+j;
                 dm_stripes_buf[idx]=dm_stripes_buf[idx]/dm_stripes_total_buf[idx];
                 // taskObj.dm_stripes[i][j] = taskObj.dm_stripes[i][j] / taskObj.dm_stripes_total[i][j];
             }
